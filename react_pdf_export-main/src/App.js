509c4d1dca0a89dbe71349df2f87c7783e5453c5
kuwinwin12341 <kuwinwin12341@gmail.com>
2023-05-30 12:24:33 +0700

    demo PDF

---
+++ react_pdf_export-main/src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { jsPDF } from "jspdf";
+import "jspdf-autotable";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then(async (data) => {
+        setData(data);
+      })
+      .then((json) => console.log(json));
+  }, []);
+
+  const exportPdf = async () => {
+    const doc = new jsPDF({ orientation: "landscape" });
+
+    doc.autoTable({
+      html: "#my-table",
+    });
+
+    doc.save("mypdf.pdf");
+  };
+
+  return (
+    <div style={{ padding: "30px" }}>
+      <button
+        className="btn btn-primary float-end mt-2 mb-2"
+        onClick={exportPdf}
+      >
+        Export Here
+      </button>
+      <h3>Table Data:</h3>
+      <table className="table table-bordered" id="my-table">
+        <thead style={{ background: "yellow" }}>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Brand</th>
+            <th scope="col">Category</th>
+            <th scope="col">Price</th>
+            <th scope="col">Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(data?.products) &&
+            data?.products?.map((row) => (
+              <tr>
+                <td>{row?.id}</td>
+                <td>{row?.title}</td>
+                <td>{row?.brand}</td>
+                <td>{row?.category}</td>
+                <td>${row?.price}</td>
+                <td>{row?.rating}/5</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
